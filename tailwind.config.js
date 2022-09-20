795ad97fd3e67a55421e34d5cce0753964345b64
exalted100 <harounayomikun@gmail.com>
2022-09-20 03:58:22 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'typathon-black': '#13191C',
+        'typathon-grey': '#BCBCBC',
+        'typathon-green': '#38A2A5',
+      }
+    },
+  },
+  plugins: [],
+}
